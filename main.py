8f8fd4ffb658979565cdb7bd7f4dbff3d5ccee5c
nikkitjiputra <115187779+nikkitjiputra@users.noreply.github.com>
2025-11-15 16:29:32 -0500

    create function

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from pymongo import MongoClient
+
+# Replace with your MongoDB connection string
+MONGODB_URL = "mongodb://localhost:27017" 
+DATABASE_NAME = "volunteer-hub"
+
+client = MongoClient(MONGODB_URL)
+database = client[DATABASE_NAME]
+collection = database["opportunities"]
+
 app = FastAPI()
 
 class Opportunity(BaseModel):
@@ -17,5 +27,8 @@ class Opportunity(BaseModel):
 
 @app.post("/submit")
 def submit_opportunity(data: Opportunity):
-    print("Received:", data.model_dump())
+    data_dict = data.model_dump()
+
+    result = collection.insert_one(data_dict)
+    print("Received:", data_dict)
     return {"status": "ok"}
